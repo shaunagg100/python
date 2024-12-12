a023054883ddfb45948fa0851638ffaef6c9bd58
shaunagg100 <shaunaggrey123@gmail.com>
2024-12-12 14:54:51 +0300

    my first commit

---
+++ shaun.py
@@ -0,0 +1,10 @@
+# file =open("python.txt" , "r")
+
+# print(file.read(15))
+# for x in file:
+#     print(x)
+
+file_write=open("python.txt" , "a")
+file_write.write("\n i love doing coding") 
+file_write.write("\n i am learning python ") 
+file_write.write("\n python is realy powerful ") 
